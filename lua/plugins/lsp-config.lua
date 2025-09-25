7c8594929e48e6524c90912719319634f3d43fed
Harrish92 <harrishan.raveendran@epfl.ch>
2025-09-26 00:40:15 +0200

    chore: rewrite lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,26 +1,26 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/nvim-cmp',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+    },
+
     config = function()
       require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
       require("mason-lspconfig").setup({
-        ensure_installed={ "lua_ls", "clangd" },
+        ensure_installed={
+          "lua_ls",
+          "clangd",
+        },
       })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { 'saghen/blink.cmp' },
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({ capabilities = capabilities})
-      lspconfig.clangd.setup({ capabilities = capabilities})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
